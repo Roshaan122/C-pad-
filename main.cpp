bdac09288a1cf9e177adad6610da71c5964eb61a
roshaanejaz122@gmail.com <roshaanejaz122@gmail.com>
2025-04-14 16:54:53 +0500

    cpad published

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<fstream> 
+#include"3C_F219311_F219110_F219213_trietree.h"
+#include"3C_F219311_F219110_F219213_huffman.h"
+#include"3C_F219311_F219110_F219213_console.h";
+using namespace std;
+int main()
+{
+	console cons;
+	cons.shopher();
+	system("pause");
+	return 0;
+
+}
